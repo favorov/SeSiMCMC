5058ff24234a486cc24d42c7d8bcc21ae93b737c
Sasha Favorov <favorov@sensi.org>
2024-08-16 01:44:24 -0400

    increased precision for ibis, to get 1 in row sum

--- src/ResultsSet.cpp
+++ src/ResultsSet.cpp
@@ -1050,7 +1050,7 @@ ostream & Profile::ibis_pwm_output (ostream & o, const string & TF, const string
             for (unsigned int letter=1;letter<=letters_in_alphabet;letter++)
             {
                 o<<setw(10)<<resetiosflags(ios::right)
-                    <<setiosflags(ios::left)<<setprecision(3);
+                    <<setiosflags(ios::left)<<setprecision(5);
                 /*if (common_background && background.size()==4)
                     o<<background[order.atgcindex(letter)-1];
                 else*/
